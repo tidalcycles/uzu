ad5f0c8f7e622a9201d83e136baf81b738827308
alex <alex@slab.org>
2024-11-23 11:31:12 +0000

    tweak style

--- packages/ohm-experiment/main.js
+++ packages/ohm-experiment/main.js
@@ -5,8 +5,7 @@ document.querySelector('#app').innerHTML = `
   <div>
     <h1>Hello</h1>
     <div class="card">
-      <textarea id="code">[bd sd].fast(2)
-      </textarea><br />
+      <textarea id="code">[bd sd].fast(2)</textarea><br />
       <button id="parse-button" type="button">parse</button>
       <div id="output">
       </div>
